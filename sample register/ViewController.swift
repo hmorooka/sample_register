f05f1b35cef273fef44a39edecb4bcd4f37f512f
hiroto morooka <h.morooka@wecnrt.co.jp>
2016-10-07 13:41:32 +0900

    firtst commit

---
+++ sample register/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample register
+//
+//  Created by 諸岡裕人 on 2016/10/07.
+//  Copyright © 2016年 hiroto.morooka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
